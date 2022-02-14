41d6d94df17187b02f9a13dd1641f23245b4fa02
ayseakbb58@gmail.com <90266532+yakupcann5@users.noreply.github.com>
2022-02-14 21:07:28 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int basamak = 0, sayi, kalan;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı Gir = ");
+        sayi = input.nextInt();
+        input.close();
+        do {
+            kalan = sayi % 10;
+            sayi = (sayi - kalan) / 10;
+            basamak++;
+        } while (sayi > 0);
+        System.out.print("Basamak Sayısı = " + basamak);
+    }
+}
